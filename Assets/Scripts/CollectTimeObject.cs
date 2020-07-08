180c24cd52d1916b56a9c074956086be62804a04
DARKCORTEX <dark_cortex2@hotmail.com>
2020-07-08 10:52:49 -0500

    Agregado Sistema de recoleccion de tiempo.

---
+++ Assets/Scripts/CollectTimeObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectTimeObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float timeCollect;
+    public AudioClip soundCollect;
+    public GameObject render;
+    public TextMesh text;
+    public float destroyTime;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollect()
+    {
+        TimeManager.OnAdjustTime(timeCollect);
+        AudioUtility.CreateSFX(soundCollect, transform.position, AudioUtility.AudioGroups.Pickup, 0f);
+        text.text = "+ " + timeCollect.ToString() + " sec";
+        render.SetActive(false);
+        text.gameObject.SetActive(true);
+        Invoke("DestroyObject",destroyTime);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            OnCollect();
+        }
+    }
+
+    public void DestroyObject()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/LookAtCam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCam : MonoBehaviour
+{
+    GameObject cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.LookAt(cam.transform.position);
+        gameObject.transform.localEulerAngles += new Vector3(0, 180, 0);
+    }
+}
